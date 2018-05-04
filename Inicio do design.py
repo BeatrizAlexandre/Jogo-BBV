92e771edae1f073faef039d9be3dfde34f012232
BeatrizAlexandre <38322321+BeatrizAlexandre@users.noreply.github.com>
2018-05-04 09:30:42 -0300

    lista de comidas fit feitas (so pra ter certeza)

--- Inicio do design.py
+++ Inicio do design.py
@@ -23,19 +23,6 @@ class ComidaFit(pygame.sprite.Sprite):
         #falta a função do movimento
     def move(self):
         self.rect.y += self.vy
-    
-class FastFood(pygame.sprite.Sprite): #Quando o usuario cortar, tem que descontar ponto
-    def __init__(self, arquivo_imagem, pos_x, pos_y, vel_x, vel_y):
-        pygame.sprite.Sprite.__init__(self)
-        self.vx = vel_x
-        self.vy = vel_y
-        self.image = pygame.image.load(arquivo_imagem)
-        self.rect = self.image.get_rect()
-        self.rect.x = pos_x
-        self.rect.y = pos_y
-        #falta a função do movimento
-    def move(self):
-        self.rect.y += self.vy
         
 pygame.init()
 tela = pygame.display.set_mode((800, 600), 0, 32)
@@ -48,7 +35,7 @@ fundo = pygame.image.load("fundo.jpg").convert()
 #cair os alimentos da lista fit:
 for comida in listafit:
     if comida == 'agua':
-        agua = ComidaFit("agua.png", randrange(600), -800, randrange(0,5),randrange(0,5))
+        agua = ComidaFit("agua.png", randrange(400), -600, randrange(0,5),randrange(0,5))
         Agua_group = pygame.sprite.Group()
         Agua_group.add(agua)
         
@@ -75,7 +62,7 @@ while rodando:
  #falta a looping principal do jogo
     agua.move()
      
- 
+
      
  
     tela.blit(fundo, (0, 0))
