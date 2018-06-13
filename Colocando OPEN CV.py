3bd2cfa2ed837e8dee55dfd795b4a2517ac9bc31
BeatrizAlexandre <38322321+BeatrizAlexandre@users.noreply.github.com>
2018-06-13 14:23:25 -0300

    jogo pronto + musica certa

--- Colocando OPEN CV.py
+++ Colocando OPEN CV.py
@@ -27,20 +27,17 @@ def message_display(text):
     pygame.display.update()
     relogio.tick(15)
   
-
+# ======== OPEN CV ========
 lowerBound = np.array([20,100,20]) #amarelo
 upperBound = np.array([80,255,255]) #amarelo
 cam = cv2.VideoCapture(0)
 
 def open_cv():
     
-
     ret, img = cam.read()
     img = cv2.resize(img, (800, 600))    
     imgHSV = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
     mask = cv2.inRange(imgHSV,lowerBound,upperBound)
-    lista_x = []
-    lista_y = []
     # Find the largest contour and extract it
     # https://stackoverflow.com/questions/39044886/finding-largest-blob-in-image
     _, contours, _ = cv2.findContours(mask,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE )
@@ -52,24 +49,24 @@ def open_cv():
         if contourSize > maxContour:
             maxContour = contourSize
             maxContourData = contour
-    if maxContourData is not None:
+    if maxContourData is not None and maxContour > 400:
         x,y,w,h=cv2.boundingRect(maxContourData)
-        x = 800 - x
+        x = 800 - x 
         ponto_x = (x + (w/2))
-        pontos_y = (y + (h/2))
+        ponto_y = (y + (h/2))
     else:
         ponto_x = None
-        pontos_y = None
+        ponto_y = None
         
-    cv2.imshow('mask',mask)
-    cv2.imshow('cam',img)
-    cv2.waitKey(10)
+#    cv2.imshow('mask',mask)
+#    cv2.imshow('cam',img)
+#    cv2.waitKey(10)
     
-    return ponto_x, pontos_y
+    return ponto_x, ponto_y
 
 
 #======== CLASSES========
-
+#Botão
 class Button(pygame.sprite.Sprite):
     def __init__(self, arquivo_imagem):
         pygame.sprite.Sprite.__init__(self)
@@ -87,7 +84,7 @@ class Button(pygame.sprite.Sprite):
             return True
         else:
             return False
-
+#Comida
 class InfoComida:
     FAST_FOOD = 0
     FIT = 1    
@@ -148,7 +145,6 @@ lista_comidas = [
 pygame.init()
 tela = pygame.display.set_mode((800, 600), 0, 32)
 
-
 gameDisplay = pygame.display.set_mode((800,600))
 pygame.display.set_caption('Fit Ninja')
 fundo_inicial = pygame.image.load("fundo.jpg").convert()
@@ -193,14 +189,15 @@ bolinha = Mouse("bolinha.png", 0, 0)
 fast_food_group = pygame.sprite.Group()
 comida_fit_group = pygame.sprite.Group()
 
+# ========= CAINDO COMIDAS ========
 ultima_pos_y = 0
-for i in range(randint(1,130)):
+for i in range(randint(1,1000)):
     c = lista_comidas[randrange(0,len(lista_comidas))]
     
     pos_x = randrange(0,600,130)
     pos_y = ultima_pos_y - randrange(200)
     ultima_pos_y = pos_y
-    rango = Comida(comidas[c].imagem, pos_x, pos_y, 3, randrange(1,3),
+    rango = Comida(comidas[c].imagem, pos_x, pos_y, 3, randrange(7,10),
                comidas[c].recompensa)
     tipo_rango = comidas[c].tipo
     if tipo_rango == InfoComida.FAST_FOOD:
@@ -209,6 +206,19 @@ for i in range(randint(1,130)):
         comida_fit_group.add(rango)
             
 
+
+
+
+# === SEGUNDA PARTE: LÓGICA DO JOGO ===
+
+relogio = pygame.time.Clock()
+
+#===== MUSICA ============
+pygame.mixer.music.load('getdown.mp3')
+
+#======= SOM ==========
+som = pygame.mixer.Sound('som.wav')
+fundo = pygame.image.load("fundo.jpg").convert()
 
 #Pontos
 pontos = 0
@@ -221,17 +231,6 @@ fonte = pygame.font.SysFont("arial", 55)
 #Recorde
 recorde = 0
 fon= pygame.font.SysFont("arial", 55)
-
-# === SEGUNDA PARTE: LÓGICA DO JOGO ===
-
-relogio = pygame.time.Clock()
-
-#===== MUSICA ============
-pygame.mixer.music.load('Baby.mp3')
-
-#======= SOM ==========
-som = pygame.mixer.Sound('som.wav')
-fundo = pygame.image.load("fundo.jpg").convert()
 
 # ========= IMAGENS =======
 right = pygame.image.load('right.png')
@@ -257,10 +256,10 @@ while estado != -1:
                 estado = -1
 
             elif event.type == MOUSEBUTTONDOWN:
-                mouse_pos = open_cv()
+                mouse_pos = pygame.mouse.get_pos()
                 if button.pressed(mouse_pos):
                     pygame.mixer.music.play(loops=-1,start=0.0)
-                    estado = 3
+                    estado = 3  
                 elif config.pressed(mouse_pos):
                      gameDisplay.blit(fundo, (0,0))
                      estado = 2
@@ -268,12 +267,16 @@ while estado != -1:
 
               
     elif estado == 1:  # Jogo começa.
+        
         for events in pygame.event.get():
             if events.type == pygame.QUIT:
                 estado = -1
         mouse_position = open_cv()
-        bolinha.move(mouse_position[0], mouse_position[1])
-                
+        if mouse_position[0] == None:
+            bolinha.move(400,300)
+        else:
+            bolinha.move(mouse_position[0], mouse_position[1])
+            
         #destruindo comidas
         pressed_keys = pygame.key.get_pressed()
         if pressed_keys[K_SPACE]:
@@ -284,15 +287,20 @@ while estado != -1:
             for comida in fast_food_killed:
                 som.play(1)
                 pontos += comida.recompensa
-#                if fast_food_killed not True:
-#                    vidas -= comida.recompensa
-                
+                                
             #PERDE VIDAS SE ACERTAR COMIDA FIT
             for comida in comida_fit_killed:  
                 pontos += comida.recompensa
                 vidas -= 1
             if vidas == 0:
                 estado = 7  # TELA GAME OVER.
+                
+        # PAUSA
+        elif event.type == MOUSEBUTTONDOWN:
+                mouse_pos = pygame.mouse.get_pos()
+                if button.pressed(mouse_pos):
+                    pygame.mixer.music.play(loops=-1,start=0.0)
+                    estado = 3 
     
         fast_food_group.update()
         comida_fit_group.update()
@@ -310,8 +318,7 @@ while estado != -1:
             estado = 7
         
         tela.blit(fundo, (0, 0))
-    
-        tela.blit(bolinha.image, (bolinha.rect.x, bolinha.rect.y))
+        
         
         #MOSTRA OS PONTOS NA TELA
         texto = font.render("Pontos: {0}". format(pontos), True, (0, 1, 0))
@@ -323,7 +330,8 @@ while estado != -1:
         
         fast_food_group.draw(tela)
         comida_fit_group.draw(tela)
-    
+        tela.blit(bolinha.image, (bolinha.rect.x, bolinha.rect.y))
+        
         pygame.display.update()
  
 
@@ -342,14 +350,14 @@ while estado != -1:
                 mouse_pos = pygame.mouse.get_pos()
                 if music.pressed(mouse_pos):
                     pygame.mixer.quit()
-                    estado = 3
+                    estado = 2
 
                 elif sound.pressed(mouse_pos):
                     pygame.mixer.quit()
                     estado = 2
                 
                 elif button.pressed(mouse_pos):
-                    pygame.mixer.music.play(loops=-1,start=0.0)
+                    #pygame.mixer.music.play(loops=-1,start=0.0)
                     estado = 1
                     
                 
@@ -418,19 +426,6 @@ while estado != -1:
             gameDisplay.blit(replay.image, replay.rect.topleft)
             tela.blit(txt2, (827 - txt.get_width() // 1, 455 - txt.get_height() // 1))
             
-            for event in pygame.event.get():
-                if event.type == QUIT:
-                    estado = -1
-                    
-                elif event.type == MOUSEBUTTONDOWN:
-                    mouse_pos = pygame.mouse.get_pos()
-                    if replay.pressed(mouse_pos):
-                        pygame.mixer.music.play(loops=-1,start=0.0)
-                        vidas = 5
-                        pontos = 0
-                        estado = 1
-        
-
             if tem_recorde:
                 texto = font.render("Novo Recorde: {0}". format(recorde), True, (0, 1, 0))
                 tela.blit(texto, (550 - texto.get_width() // 1, 250 - texto.get_height() // 1))
@@ -439,8 +434,18 @@ while estado != -1:
                 texto = font.render("Pontos: {0}". format(pontos), True, (0, 1, 0))
                 tela.blit(texto, (500 - texto.get_width() // 1, 250 - texto.get_height() // 1))
             
-            
-              
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    estado = -1
+                    
+                elif event.type == MOUSEBUTTONDOWN:
+                    mouse_pos = pygame.mouse.get_pos()
+                    if replay.pressed(mouse_pos):
+                        pygame.mixer.music.play(loops=-1,start=0.0)
+                        estado = 1
+                        vidas = 5
+                        pontos = 0
+   
             pygame.display.update()
         
 
